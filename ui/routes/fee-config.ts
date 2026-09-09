d0431ae1298f5e0a079c73d4b238eb4848cffa1a
handsdiff <239876380+handsdiff@users.noreply.github.com>
2026-09-09 15:59:08 -0400

    elsewhere

--- ui/routes/fee-config.ts
+++ ui/routes/fee-config.ts
@@ -61,6 +61,7 @@ export const PARTNER_REFERRED_DAO_PDAS: Set<string> = new Set([
   '4YrUf7URPvb6hFYuw8DSXGwtuwGB2Xn4p3MUJAULLANz',
   'FQN8KF6Yy2VDCWJkUyMraDUo5Qk5oaMuSbfQ4rxXyqJ8',
   'kUU645mYqeKNMthKwQUMwrXHvtbr4zDzJNVEbVSwtLH',
+  'AK8sdprP6mQAAzJr8cWag8b3EMXSfC9JiFiotHb99fVR',
 ]);
 
 // DAOs directly managed by the partner get split:
